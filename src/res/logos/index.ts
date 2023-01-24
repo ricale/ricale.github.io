2ae885525e99346d4e701cba8939b92e18d6c8ac
KIM Kangseong <kim.kangseong@gmail.com>
2023-01-24 14:50:23 +0900

    블로그 링크 업데이트

--- src/res/logos/index.ts
+++ src/res/logos/index.ts
@@ -1,4 +1,4 @@
+export { ReactComponent as gatsby } from './gatsby.svg';
 export { ReactComponent as github } from './github.svg';
 export { ReactComponent as mail } from './mail.svg';
 export { ReactComponent as linkedin } from './linkedin.svg';
-export { ReactComponent as velog } from './velog.svg';
